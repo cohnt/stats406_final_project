3e85b9228f36201330c0191deabcdabb5886a930
Thomas Cohn <cohnt@umich.edu>
2019-10-25 01:57:54 -0400

    Wrote an analogous script for multiple hurricanes.

--- python/visualization/track.py
+++ python/visualization/track.py
@@ -65,3 +65,25 @@ def plot_track_status(hurricane):
 		m.plot(x, y, latlon=True, color=c)
 
 	plt.show()
+
+def plot_tracks_status(hurricane_list):
+	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
+	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
+	            resolution ='l',area_thresh=1000.)
+	m.drawcoastlines()
+	m.drawcountries()
+	m.drawmapboundary(fill_color='#99ffff')
+	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
+	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+
+	for hurricane in hurricane_list:
+		for i in range(0, len(hurricane.track)-1):
+			x = [hurricane.track[i][0], hurricane.track[i+1][0]]
+			y = [hurricane.track[i][1], hurricane.track[i+1][1]]
+			c = "blue"
+			if hurricane.data_lines[i].system_status == "HU":
+				c = "red"
+			m.plot(x, y, latlon=True, color=c)
+
+	plt.show()
