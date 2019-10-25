447c92fa041b00cc8a04f8d27d1a01d3a2bd77b6
Thomas Cohn <cohnt@umich.edu>
2019-10-25 01:56:33 -0400

    Switch to using latlon=True instead of transforming points myself. Wrote something to plot a storm and highlight when it's a hurricane.

--- python/visualization/track.py
+++ python/visualization/track.py
@@ -24,15 +24,11 @@ def plot_track_map(track):
 	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
 	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
 
-	map_pts_x, map_pts_y = m(track[:,0], track[:,1])
-	m.plot(map_pts_x, map_pts_y)
+	m.plot(track[:,0], track[:,1], latlon=True)
 
 	plt.show()
 
 def plot_tracks_map(track_list):
-	# Heavily based on:
-	# https://matplotlib.org/basemap/users/mapcoords.html
-	# https://matplotlib.org/basemap/users/examples.html
 	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
 	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
 	            resolution ='l',area_thresh=1000.)
@@ -45,7 +41,27 @@ def plot_tracks_map(track_list):
 
 	for track in track_list:
 		track = np.asarray(track)
-		map_pts_x, map_pts_y = m(track[:,0], track[:,1])
-		m.plot(map_pts_x, map_pts_y)
+		m.plot(track[:,0], track[:,1], latlon=True)
+
+	plt.show()
+
+def plot_track_status(hurricane):
+	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
+	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
+	            resolution ='l',area_thresh=1000.)
+	m.drawcoastlines()
+	m.drawcountries()
+	m.drawmapboundary(fill_color='#99ffff')
+	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
+	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+
+	for i in range(0, len(hurricane.track)-1):
+		x = [hurricane.track[i][0], hurricane.track[i+1][0]]
+		y = [hurricane.track[i][1], hurricane.track[i+1][1]]
+		c = "blue"
+		if hurricane.data_lines[i].system_status == "HU":
+			c = "red"
+		m.plot(x, y, latlon=True, color=c)
 
 	plt.show()
