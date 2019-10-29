6fa2d751fc45149423827f594475d7e7568dcbf3
Thomas Cohn <cohnt@umich.edu>
2019-10-29 15:14:03 -0400

    Move the generation of a default map to its own function.

--- python/visualization/track.py
+++ python/visualization/track.py
@@ -11,33 +11,13 @@ def plot_track_raw(track):
 def plot_track_map(track):
 	track = np.asarray(track)
 	
-	# Heavily based on:
-	# https://matplotlib.org/basemap/users/mapcoords.html
-	# https://matplotlib.org/basemap/users/examples.html
-	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=-20.,urcrnrlat=57.,
-	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
-	            resolution ='l',area_thresh=1000.)
-	m.drawcoastlines()
-	m.drawcountries()
-	m.drawmapboundary(fill_color='#99ffff')
-	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
-	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
-
+	m = create_default_map()
 	m.plot(track[:,0], track[:,1], latlon=True)
 
 	plt.show()
 
 def plot_tracks_map(track_list):
-	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
-	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
-	            resolution ='l',area_thresh=1000.)
-	m.drawcoastlines()
-	m.drawcountries()
-	m.drawmapboundary(fill_color='#99ffff')
-	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
-	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+	m = create_default_map()
 
 	for track in track_list:
 		track = np.asarray(track)
@@ -46,15 +26,7 @@ def plot_tracks_map(track_list):
 	plt.show()
 
 def plot_track_status(hurricane):
-	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
-	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
-	            resolution ='l',area_thresh=1000.)
-	m.drawcoastlines()
-	m.drawcountries()
-	m.drawmapboundary(fill_color='#99ffff')
-	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
-	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+	m = create_default_map()
 
 	for i in range(0, len(hurricane.track)-1):
 		x = [hurricane.track[i][0], hurricane.track[i+1][0]]
@@ -67,15 +39,7 @@ def plot_track_status(hurricane):
 	plt.show()
 
 def plot_tracks_status(hurricane_list):
-	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
-	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
-	            resolution ='l',area_thresh=1000.)
-	m.drawcoastlines()
-	m.drawcountries()
-	m.drawmapboundary(fill_color='#99ffff')
-	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
-	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+	m = create_default_map()
 
 	for hurricane in hurricane_list:
 		for i in range(0, len(hurricane.track)-1):
@@ -87,3 +51,18 @@ def plot_tracks_status(hurricane_list):
 			m.plot(x, y, latlon=True, color=c)
 
 	plt.show()
+
+def create_default_map():
+	# Heavily based on:
+	# https://matplotlib.org/basemap/users/mapcoords.html
+	# https://matplotlib.org/basemap/users/examples.html
+	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
+	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
+	            resolution ='l',area_thresh=1000.)
+	m.drawcoastlines()
+	m.drawcountries()
+	m.drawmapboundary(fill_color='#99ffff')
+	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
+	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+	return m
