4646414b720dc4d1432c2f5ae125ebd1ec0c5d99
Thomas Cohn <cohnt@umich.edu>
2019-10-29 16:47:54 -0400

    Option to specify a basemap for each plot, and create a new map (just of florida).

--- python/visualization/track.py
+++ python/visualization/track.py
@@ -9,13 +9,17 @@ def plot_track_raw(track):
 	ax.scatter(track[:,0], track[:,1])
 	plt.show()
 
-def plot_track_map(track, linewidth=2):
+def plot_track_map(track, m=None, linewidth=2):
+	if m == None:
+		m=create_default_map()
+
 	track = np.asarray(track)
 
-	plot_tracks_map([track], linewidth=linewidth)
+	plot_tracks_map([track], m, linewidth=linewidth)
 
-def plot_tracks_map(track_list, linewidth=2):
-	m = create_default_map()
+def plot_tracks_map(track_list, m=None, linewidth=2):
+	if m == None:
+		m=create_default_map()
 
 	for track in track_list:
 		track = np.asarray(track)
@@ -23,13 +27,15 @@ def plot_tracks_map(track_list, linewidth=2):
 
 	plt.show()
 
-def plot_track_status(hurricane, linewidth=2):
-	m = create_default_map()
+def plot_track_status(hurricane, m=None, linewidth=2):
+	if m == None:
+		m=create_default_map()
 
-	plot_tracks_status([hurricane], linewidth=linewidth)
+	plot_tracks_status([hurricane], m, linewidth=linewidth)
 
-def plot_tracks_status(hurricane_list, linewidth=2):
-	m = create_default_map()
+def plot_tracks_status(hurricane_list, m=None, linewidth=2):
+	if m == None:
+		m=create_default_map()
 
 	for hurricane in hurricane_list:
 		for i in range(0, len(hurricane.track)-1):
@@ -42,9 +48,11 @@ def plot_tracks_status(hurricane_list, linewidth=2):
 
 	plt.show()
 
-def plot_tracks_wind_speed(hurricane_list, color_max_wind=120.0, linewidth=2):
+def plot_tracks_wind_speed(hurricane_list, m=None, color_max_wind=120.0, linewidth=2):
+	if m == None:
+		m=create_default_map()
+
 	# color_max_wind is the highest wind speed which is displayed as its own color
-	m = create_default_map()
 
 	for hurricane in hurricane_list:
 		for i in range(0, len(hurricane.track)-1):
@@ -60,6 +68,21 @@ def create_default_map():
 	# https://matplotlib.org/basemap/users/mapcoords.html
 	# https://matplotlib.org/basemap/users/examples.html
 	m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,urcrnrlon=20.,urcrnrlat=60.,
+	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
+	            resolution ='l',area_thresh=1000.)
+	m.drawcoastlines()
+	m.drawcountries()
+	m.drawmapboundary(fill_color='#99ffff')
+	m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+	m.drawparallels(np.arange(10, 70, 20),labels=[1, 1, 0, 0])
+	m.drawmeridians(np.arange(-100, 0, 20),labels=[0, 0, 0, 1])
+	return m
+
+def create_gulf_map():
+	pass
+
+def create_florida_map():
+	m = Basemap(llcrnrlon=-85.,llcrnrlat=20.,urcrnrlon=-75.,urcrnrlat=35.,
 	            projection='lcc',lat_1=20.,lat_2=40.,lon_0=-60.,
 	            resolution ='l',area_thresh=1000.)
 	m.drawcoastlines()
