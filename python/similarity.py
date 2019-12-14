45e3d51d3f8c27fc93e940541de268e2efa21960
Thomas Cohn <cohnt@umich.edu>
2019-12-14 05:19:17 -0500

    Function stubs for similarity metrics on variable-length time series.

---
+++ python/similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def M1(A, B, delta, eps_vec):
+	pass
+
+def M2(A, B, delta, eps_vec):
+	pass
+
+def LCSS(A, B, delta, eps_vec):
+	pass
+
+def SLC(A, B, delta, eps_vec):
+	pass
+
+def H(A):
+	pass
