c4efd467657f31624beb3e448ecf2961afa02bc3
Thomas Cohn <cohnt@umich.edu>
2019-12-14 06:02:11 -0500

    Quick script to demonstrate similarity of distinct hurricanes.

---
+++ python/scripts/similarity_test_2.py
@@ -0,0 +1,17 @@
+import sys
+
+import read_data
+from visualization.track import *
+from visualization.storm_lifetime import *
+from simulation.simulation import *
+from similarity import *
+
+hurricane_list = read_data.read_file("../data/by_year/2018.csv")[0:2]
+
+plot_tracks_status(hurricane_list)
+
+ts1 = hurricane_to_time_series(hurricane_list[0])
+ts2 = hurricane_to_time_series(hurricane_list[1])
+
+for eps in range(1, 50):
+	print M1(ts1, ts2, 21600, eps)
