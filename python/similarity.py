f59cb62e525da2d1b67c9ee81116ec55afeafa14
Thomas Cohn <cohnt@umich.edu>
2019-12-14 05:55:33 -0500

    Just make them numpy arrays at the start so I don't have to keep converting.

--- python/similarity.py
+++ python/similarity.py
@@ -6,7 +6,7 @@ def datetime_to_epoch(dt):
 
 def hurricane_to_time_series(hurricane):
 	time_vec = [datetime_to_epoch(hurricane.data[i].time) for i in range(len(hurricane.data))]
-	return [(time_vec[i]-time_vec[0], hurricane.track[i]) for i in range(len(hurricane.track))]
+	return [(time_vec[i]-time_vec[0], np.array(hurricane.track[i])) for i in range(len(hurricane.track))]
 
 def M1(A, B, delta, eps):
 	l1 = len(A)
@@ -26,7 +26,7 @@ def LCSS(A, B, delta, eps):
 		for i in range(1, len(A)+1):
 			for j in range(1, len(B)+1):
 				# Check time distance and real distance
-				if abs(A[i][0]-B[j][0]) < delta and norm(np.asarray(A[i][1])-np.asarray(B[j][1])) < eps:
+				if abs(A[i][0]-B[j][0]) < delta and norm(A[i][1]-B[j][1]) < eps:
 					arr[i][j] = arr[i-1][j-1] + 1
 				else:
 					arr[i][j] = np.max(arr[i-1][j], arr[i][j-1])
