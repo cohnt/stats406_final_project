0fd6bb9998f28f79a4329d121197b73ff426d756
Thomas Cohn <cohnt@umich.edu>
2019-12-14 05:42:42 -0500

    Dynamic programming LCSS.

--- python/similarity.py
+++ python/similarity.py
@@ -8,20 +8,31 @@ def hurricane_to_time_series(hurricane):
 	time_vec = [datetime_to_epoch(hurricane.data[i].time) for i in range(len(hurricane.data))]
 	return [(time_vec[i], hurricane.track[i]) for i in range(len(hurricane.track))]
 
-def M1(A, B, delta, eps_vec):
+def M1(A, B, delta, eps):
 	l1 = len(A)
 	l2 = len(B)
 	return LCSS(A, B, delta, eps_vec) / np.min(l1, l2)
 
-def M2(A, B, delta, eps_vec):
+def M2(A, B, delta, eps):
 	l1 = len(A)
 	l2 = len(B)
 	return SLC(A, B, delta, eps_vec) / np.min(l1, l2)
 
-def LCSS(A, B, delta, eps_vec):
-	pass
+def LCSS(A, B, delta, eps):
+	if len(A) == 0 or len(B) == 0:
+		return 0
+	else:
+		arr = np.zeros(len(A)+1, len(B)+1)
+		for i in range(1, len(A)+1):
+			for j in range(1, len(B)+1):
+				# Check time distance and real distance
+				if abs(A[i][0]-B[j][0]) < delta and norm(A[i][1]-B[j][1]) < eps:
+					arr[i][j] = arr[i-1][j-1] + 1
+				else:
+					arr[i][j] = np.max(arr[i-1][j], arr[i][j-1])
+		return arr[len(A), len(B)]
 
-def SLC(A, B, delta, eps_vec):
+def SLC(A, B, delta, eps):
 	pass
 
 def H(A):
