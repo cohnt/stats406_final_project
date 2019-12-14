6d90bbb891f8143c38199742e691368cd63f8432
Thomas Cohn <cohnt@umich.edu>
2019-12-14 06:07:36 -0500

    Implement SLC.

--- python/similarity.py
+++ python/similarity.py
@@ -33,7 +33,17 @@ def LCSS(A, B, delta, eps):
 		return arr[len(A), len(B)]
 
 def SLC(A, B, delta, eps):
-	pass
-
-def H(A):
-	pass
+	if len(A) == 0 or len(B) == 0:
+		return 0
+	else:
+		arr = np.zeros((len(A)+1, len(B)+1))
+		for i in range(1, len(A)+1):
+			for j in range(1, len(B)+1):
+				# Check time distance and real distance
+				if abs(A[i-1][0]-B[j-1][0]) < delta and np.linalg.norm(A[i-1][1]-B[j-1][1]) < eps:
+					C = eps - np.linalg.norm(A[i-1][1]-B[j-1][1])
+					const = np.min([1, 1-(C/eps)])
+					arr[i][j] = arr[i-1][j-1] + const
+				else:
+					arr[i][j] = np.max([arr[i-1][j], arr[i][j-1]])
+		return arr[len(A), len(B)]
