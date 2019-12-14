993123811fe1113cd1aa367b95152feb009004e5
Thomas Cohn <cohnt@umich.edu>
2019-12-14 06:00:17 -0500

    Fix indexing issues, and problems with how I call np.min and np.max.

--- python/similarity.py
+++ python/similarity.py
@@ -11,12 +11,12 @@ def hurricane_to_time_series(hurricane):
 def M1(A, B, delta, eps):
 	l1 = len(A)
 	l2 = len(B)
-	return LCSS(A, B, delta, eps) / np.min(l1, l2)
+	return LCSS(A, B, delta, eps) / np.min([l1, l2])
 
 def M2(A, B, delta, eps):
 	l1 = len(A)
 	l2 = len(B)
-	return SLC(A, B, delta, eps) / np.min(l1, l2)
+	return SLC(A, B, delta, eps) / np.min([l1, l2])
 
 def LCSS(A, B, delta, eps):
 	if len(A) == 0 or len(B) == 0:
@@ -26,10 +26,10 @@ def LCSS(A, B, delta, eps):
 		for i in range(1, len(A)+1):
 			for j in range(1, len(B)+1):
 				# Check time distance and real distance
-				if abs(A[i][0]-B[j][0]) < delta and np.linalg.norm(A[i][1]-B[j][1]) < eps:
+				if abs(A[i-1][0]-B[j-1][0]) < delta and np.linalg.norm(A[i-1][1]-B[j-1][1]) < eps:
 					arr[i][j] = arr[i-1][j-1] + 1
 				else:
-					arr[i][j] = np.max(arr[i-1][j], arr[i][j-1])
+					arr[i][j] = np.max([arr[i-1][j], arr[i][j-1]])
 		return arr[len(A), len(B)]
 
 def SLC(A, B, delta, eps):
