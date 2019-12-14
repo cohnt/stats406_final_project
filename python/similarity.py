41fe3c82f23ce71af7549da0837cc1352047f248
Thomas Cohn <cohnt@umich.edu>
2019-12-14 05:32:30 -0500

    Helper functions to convert stuff to time series.

--- python/similarity.py
+++ python/similarity.py
@@ -1,4 +1,12 @@
 import numpy as np
+from datetime import datetime
+
+def datetime_to_epoch(dt):
+	return (dt - datetime(1970, 1, 1)).total_seconds()
+
+def hurricane_to_time_series(hurricane):
+	time_vec = [datetime_to_epoch(hurricane.data[i].time) for i in range(len(hurricane.data))]
+	return [(time_vec[i], hurricane.track[i]) for i in range(len(hurricane.track))]
 
 def M1(A, B, delta, eps_vec):
 	pass
