5192111e66951cab4592d4fca7981650e855ed3e
Thomas Cohn <cohnt@umich.edu>
2019-12-20 07:08:02 -0500

    Try a larger covariance.

--- python/scripts/noise_test_script.py
+++ python/scripts/noise_test_script.py
@@ -9,12 +9,12 @@ hurricane_list = [read_data.read_file("../data/by_year/2018.csv")[0]]
 
 plot_tracks_status(hurricane_list)
 
-repeated_list = repeatDeepCopy(hurricane_list, 10)
+repeated_list = repeatDeepCopy(hurricane_list, 10, cov=0.4*np.eye(2))
 addTrackNoise(repeated_list)
 
 plot_tracks_status(repeated_list)
 
-repeated_list = repeatDeepCopy(hurricane_list, 10)
+repeated_list = repeatDeepCopy(hurricane_list, 10, cov=0.4*np.eye(2))
 pointwiseTrackNoise(repeated_list)
 
 plot_tracks_status(repeated_list)
