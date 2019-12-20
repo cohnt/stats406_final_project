fda2baad28ba27750325dbc43c77d880a8fcb26b
Thomas Cohn <cohnt@umich.edu>
2019-12-20 07:07:47 -0500

    Modify the script to give me some images I need.

--- python/scripts/noise_test_script.py
+++ python/scripts/noise_test_script.py
@@ -5,16 +5,16 @@ from visualization.track import *
 from visualization.storm_lifetime import *
 from simulation.simulation import *
 
-hurricane_list = read_data.read_file("../data/by_year/2018.csv")
+hurricane_list = [read_data.read_file("../data/by_year/2018.csv")[0]]
 
 plot_tracks_status(hurricane_list)
 
-repeated_list = repeatDeepCopy(hurricane_list, 1)
+repeated_list = repeatDeepCopy(hurricane_list, 10)
 addTrackNoise(repeated_list)
 
 plot_tracks_status(repeated_list)
 
-repeated_list = repeatDeepCopy(hurricane_list, 1)
+repeated_list = repeatDeepCopy(hurricane_list, 10)
 pointwiseTrackNoise(repeated_list)
 
 plot_tracks_status(repeated_list)
